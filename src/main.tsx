d44461178814094935555c1de640bfc686bc256f
Shlaampe <piphencova@icloud.com>
2024-10-06 15:29:50 +0300

    Changed file paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from '../src/App'
-import '../src/input.css'
+import App from './App'
+import './input.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
