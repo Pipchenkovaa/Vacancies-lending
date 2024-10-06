80a1618cdd15c7b0b057cd1131495e9f0cd2de82
Shlaampe <piphencova@icloud.com>
2024-10-06 16:41:26 +0300

    Some paths changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Page2 } from './page-2/main-page-2';
 
 function App() {
 	return (
-		<Router>
+		<Router basename="/Vacancies-lending/">
 			<div className="container mx-auto pb-6">
 				<Routes>
 					<Route path="/" element={<Page1 />} />
@@ -12,7 +12,7 @@ function App() {
 				</Routes>
 			</div>
 		</Router>
-	)
+	);
 }
 
 export default App;
