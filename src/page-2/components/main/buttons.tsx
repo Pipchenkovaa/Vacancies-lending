392347c4325246b96cae0701aade335f3fc990c5
Shlaampe <piphencova@icloud.com>
2024-10-07 15:43:05 +0300

    Changed button's sizes

--- src/page-2/components/main/buttons.tsx
+++ src/page-2/components/main/buttons.tsx
@@ -1,8 +1,8 @@
 export function CreateButtons() {
 	return (
-		<div className="button-wrapper flex justify-end items-end py-3 px-4 gap-4 text-[15px] text-white font-['Commissioner'] font-bold pb-4">
-			<p className="bg-[#523829] shadow-md hover:bg-[#A2866E] hover:shadow-myInner hover:shadow-[#714932] px-12 lg:px-9 xl:px-12 py-3 rounded-2xl uppercase transition-all duration-400">Подробнее о компании</p>
-			<p className="bg-[#523829] shadow-md hover:bg-[#A2866E] hover:shadow-myInner hover:shadow-[#714932] px-12 lg:px-9 xl:px-12 py-3 rounded-2xl uppercase transition-all duration-400">Откликнуться</p>
+		<div className="button-wrapper flex justify-center sm:justify-end items-end py-3 px-4 gap-4 text-[15px] text-white font-['Commissioner'] font-bold pb-4">
+			<p className="bg-[#523829] shadow-md hover:bg-[#A2866E] hover:shadow-myInner hover:shadow-[#714932] px-6 sm:px-8 lg:px-9 xl:px-12 py-3 rounded-2xl uppercase transition-all duration-400">Подробнее о компании</p>
+			<p className="bg-[#523829] shadow-md hover:bg-[#A2866E] hover:shadow-myInner hover:shadow-[#714932] px-6 sm:px-8 lg:px-9 xl:px-12 py-3 rounded-2xl uppercase transition-all duration-400">Откликнуться</p>
 		</div>
 	)
 }
